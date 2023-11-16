e898727c93488a446a677f4a119d526be3db4821
Joseph Young <musicjoeyoung@gmail.com>
2023-11-16 15:40:51 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+Netlify Image CDN Beta - Test
+<img src="https://mysitename.netlify.app/.netlify/images?url=/cat1.jpeg"/>
+    </>
+  )
+}
+
+export default App
