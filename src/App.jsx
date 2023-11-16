45d43c7c967d8456ef9e40902aa057674471eb1b
Joseph Young <musicjoeyoung@gmail.com>
2023-11-16 15:46:03 -0500

    src

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-
 function App() {
 
   return (
     <>
 Netlify Image CDN Beta - Test
-<img src="https://mysitename.netlify.app/.netlify/images?url=/cat1.jpeg"/>
+<img src="https://superlative-brigadeiros-2fab95.netlify.app/.netlify/images?url=src/assets/cat1.jpeg"/>
     </>
   )
 }
